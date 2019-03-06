71900fd7599f9377c03170c303471c8a6cb0f267
Travis Martin <travisemerymartin@gmail.com>
2019-03-06 23:05:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,7 +98,8 @@ def all_supplies_in_holidays(holiday_hash)
     cap_season=season.to_s.capitalize!
     puts cap_season+":"
     days.each do |day, supply|
-      cap_day=day.to_s.to_a.split("_")
+      cap_day=day.to_s
+      cap_day=[*cap_day].split("_")
       cap_day.map {|d| d=d.to_s.captialize!}
       cap_supply=[]
       day.each {|s| cap_supply<<s.to_s.capitalize!}
